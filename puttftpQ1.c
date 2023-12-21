aaac61f10a80de9f0c1c9c3f0011e2d2ebd40586
LucasZamb <151966938+LucasZamb@users.noreply.github.com>
2023-12-21 08:51:44 +0100

    Add files via upload

---
+++ puttftpQ1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>	
+
+
+int main(int argc, char *argv[]) {
+    char *server;
+    char *filename;
+
+	//function syntax reminder
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server> <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    server = argv[1];
+    filename = argv[2];
+
+    printf("Server: %s\n", server);
+    printf("File: %s\n", filename);
+
+    return 0;
+}
