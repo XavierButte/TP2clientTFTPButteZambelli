11a25dc25e5912f2aa03f95b0035b1703c59ee93
LucasZamb <151966938+LucasZamb@users.noreply.github.com>
2023-12-21 11:49:59 +0100

    Add files via upload

--- gettftpQ3.c
+++ gettftpQ3.c
@@ -4,61 +4,51 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
+#include <sys/types.h>
 
 
 int main(int argc, char *argv[]) {
-    char *server;
-    char *filename;
-
-
-	//function syntax reminder
-    if (argc != 3) {
+     if (argc != 3) {
         fprintf(stderr, "Usage: %s <server> <file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
-    
-    server = argv[1];
-    filename = argv[2];
-    
-    
-    struct addrinfo hints, *servinfo, *p;
-    int status;
-    int sockfd;
 
+    char *server = argv[1];
+    char *filename = argv[2];
+    char service[NI_MAXSERV];
+    
+    
+
+    
+    struct addrinfo hints, *result;
+    int sockfd;
+    memset(&hints, 0, sizeof hints);       //set hints to 0
+    hints.ai_family = AF_UNSPEC;           //ai_family field definition
+    hints.ai_socktype = SOCK_DGRAM;        //definition of the socket type used
+	hints.ai_flags = AI_PASSIVE;           //for wildcard IP address
+	hints.ai_protocol = IPPROTO_UDP;       //any pprotocole
+	
+	
     printf("Server: %s\n", server);
     printf("File: %s\n", filename);
-    
-    
-    memset(&hints, 0, sizeof hints);           //set hints to 0
-    hints.ai_family = AF_UNSPEC;               //ai_family field definition
-    hints.ai_socktype = SOCK_DGRAM; 		   //definition of the socket type used
-
-    status= getaddrinfo(server, NULL, &hints, &servinfo);
-    if ( status != 0) {
+   
+    int status = getaddrinfo(server, TFTP_PORT, &hints, &result);
+    if (status != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         return 1;
     }
-    
-    
-     for(p = servinfo; p != NULL; p = p->ai_next) {
-        //socket creation
-        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (sockfd == -1) {
-            perror("client: socket");
-            continue;
-        }
 
-        break; //if socket creation is successful, exit the loop.
-    }
-    
-        if (p == NULL) {
-        fprintf(stderr, "client: failed to create socket\n");
-        return 2;
-    }
-    
-    
-    freeaddrinfo(servinfo);          //frees space allocated by getaddrinfo()
+
+        //socket creation
+     sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+     if (sockfd == -1) {
+         perror("error");
+         exit(EXIT_FAILURE);
+        }
+    printf("socket :%d \n", sockfd);
+
+    freeaddrinfo(result);          //frees space allocated by getaddrinfo()
+    close(sockfd);
 
     return 0;
 }
